34d49d810f1897d8381322d082df7cf360942d05
Hoang Viet <viethxse160082@fpt.edu.vn>
2023-02-26 09:57:50 +0700

    model

---
+++ src/java/model/admin.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author chotr
+ */
+public class admin extends account{
+    private int id;
+    private String name;
+
+    public admin() {
+    }
+
+    public admin(int id, String name, String user, String password) {
+        super(user, password);
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
---
+++ src/java/model/order.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author chotr
+ */
+public class order {
+    private int id;
+    private int id_transaction;
+    private int status;
+
+    public order(int id, int id_transaction, int status) {
+        this.id = id;
+        this.id_transaction = id_transaction;
+        this.status = status;
+    }
+
+    public order() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_transaction() {
+        return id_transaction;
+    }
+
+    public void setId_transaction(int id_transaction) {
+        this.id_transaction = id_transaction;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+    
+}
